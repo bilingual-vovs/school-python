b0c6da1e1754d9a669d2dc746798ef03d837892c
bilingual-vovs <vovavovsart@gmail.com>
2022-09-27 20:23:41 +0300

    Classwork

---
+++ Classwork_27.09.py/Task_1.py
@@ -0,0 +1,9 @@
+a = int(input("Enter x1: "))
+b = int(input("Enter y1: "))
+x = int(input("Enter x2: "))
+y = int(input("Enter y2: "))
+
+if (a-x + b-y)%2 == 0: 
+    print("Yes")
+else:
+    print("No")
