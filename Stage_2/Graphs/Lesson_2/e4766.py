9711f136a7c621d12843d9179816b8609c31c03b
bilingual-vovs <vovavovsart@gmail.com>
2024-01-14 12:49:34 +0200

    BFS

--- Stage_2/Graphs/Lesson_2/e4766.py
+++ Stage_2/Graphs/Lesson_2/e4766.py
@@ -5,7 +5,7 @@ for y in range(n):
     inp = input().split()
     for x in range(n):
         if inp[x] == '1':
-            l.append(y, x)
+            l.append((y+1, x+1))
     
 for x in l:
     print(f'{x[0]} {x[1]}')
