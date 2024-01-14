28c36d93ff6bf69cf80423cdde711db42afbd2a4
bilingual-vovs <vovavovsart@gmail.com>
2024-01-14 11:57:48 +0200

    Not solved 2157

--- Stage_2/Graphs/DFS_algorithm/e2157.py
+++ Stage_2/Graphs/DFS_algorithm/e2157.py
@@ -2,41 +2,43 @@ n = int(input())
 
 matrix = [[0 for _ in range(n)] for _ in range(n)]
 
-counters = [0 for _ in range(n)]
-
 for _ in range(n-1):
     i = [int(x) for x in input().split()]
     matrix[i[0]-1][i[1]-1] = matrix[i[1]-1][i[0]-1] = i[2]
 
-    counters[i[0]-1] += 1
-    counters[i[1]-1] += 1
+node = [0]
+edges = []
+summ = 0
 
 for x in range(n):
-    if counters[x] == 1:
-        corner = x
-        break
-
-path = [corner]
-suma = 0
-for x in range(n):
-        edge = matrix[path[-1]][x]
-        if edge and type(edge) is int:
-            matrix[x][path[-1]] = [edge, len(path)]
-            path.append(x)
-
-while path[-1] != corner :
-    aded = False
+        if matrix[node[-1]][x]:
+            for e in range(len(edges)):
+                edges[e][1]+=1
+            edges.append([matrix[node[-1]][x], 1]) 
+            matrix[node[-1]][x] = matrix[x][node[-1]] = 0
+            node.append(x)
+            break 
+        
+while len(node) :
+    f = False
     for x in range(n):
-        edge = matrix[path[-1]][x]
-        if edge and type(edge) is int:
-            matrix[x][path[-1]] = [edge, len(path)+1]
-            path.append(x)
-            aded = True
-    if not aded:
-        matrix[path[-2]][path[-1]] = [matrix[path[-2]][path[-1]], len(path)-1]
-        path.pop(-1)
+        if matrix[node[-1]][x]:
+            for e in range(len(edges)):
+                edges[e][1]+=1
+            edges.append([matrix[node[-1]][x], 1]) 
+            matrix[node[-1]][x] = matrix[x][node[-1]] = 0
+            node.append(x)
+            f = True
+            break 
+    if f: continue
+    if len(edges):
+        edge = edges.pop(-1)
+        summ += edge[0] * edge[1] * (n-edge[1])
+    node.pop(-1)
+    
+
+    
 
 
 
-for x in matrix:
-    print(x)
+print(summ)
