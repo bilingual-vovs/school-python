d2acd7d05f6c4979325557f74d8a469d76719e38
bilingual-vov <vovavovsart@gmail.com>
2024-01-25 10:55:16 +0200

    Exams

---
+++ Stage_2/Graphs/Dejkstra_algorithm/Exam.py
@@ -0,0 +1,31 @@
+inp = input().split()
+n = int(inp[0])
+to = int(inp[1])
+s = 0
+
+matrix = []
+for _ in range(n):
+    matrix.append([int(x) if x != '10000' else 0 for x in input().split(' ')])
+
+
+dejlist = [float('inf') for _ in range(n)]
+dejlist[s] = 0
+dejfix = [-1 for _ in range(n)]
+dejfix[s] = 0
+act = s 
+
+for x in range(n-1):
+    for y in range(n):
+        if matrix[act][y] and dejfix[y] == -1:
+            dejlist[y] = min(dejlist[y], dejfix[act] + matrix[act][y])
+    i = 0
+    minm = float('inf')
+    for y in range(n):
+        if minm > dejlist[y] and dejfix[y] == -1:
+            minm = dejlist[y]
+            indx = i
+        i+=1
+    act = indx
+    dejfix[act] = minm
+
+print(dejfix[to-1])
