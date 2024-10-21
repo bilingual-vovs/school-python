315196a08385a0e18b6ff5c24ad3d72dcce754cf
bilingual-vovs <vovavovsart@gmail.com>
2024-10-21 14:46:13 +0300

    Ol

--- Year_10/olymp_1.py
+++ Year_10/olymp_1.py
@@ -1,22 +1,28 @@
 n = int(input())
 
-def minN(ln):
-    r = 1
-    i = 1
-    while r+i <= ln:
-        r+=i
-        i+=1
-    return i
+if n == 0:
+    print(-1)
+else:
 
-b = [minN(x) for x in range(1,n+2)]
+    def minN(ln):
+        r = 1
+        i = 1
+        while r+i <= ln:
+            r+=i
+            i+=1
+        return i
 
-m = [[0 for _ in range(n+1)] for _ in range(n+1)]
-m[1][1] = 1
-m[2][2] = 1
-m[0][0] = 1
+    b = [minN(x) for x in range(1,n+2)]
 
-for ln in range(2,n+1):
-    for bs in range(b[ln-1],ln+1):
-        g = m[n-bs][slice(0, bs)]
-        m[ln][bs] = sum(m[ln-bs][slice(0, bs)])
-print(sum(m[-1]))      
+    m = [[0 for _ in range(n+1)] for _ in range(n+1)]
+
+    if n >= 2:  
+        m[1][1] = 1
+        m[2][2] = 1
+    m[0][0] = 1
+
+    for ln in range(2,n+1):
+        for bs in range(b[ln-1],ln+1):
+            g = m[n-bs][slice(0, bs)]
+            m[ln][bs] = sum(m[ln-bs][slice(0, bs)])
+    print(sum(m[-1]))      
